4f3773d170545384cc48ee7b428bc865d6fa4db0
NatNichols <natalie.e.nichols@gmail.com>
2024-01-11 23:30:46 -0500

    remove data directory

--- data/candy-data.js
+++
@@ -1,9 +0,0 @@
-const candies = [
-  {text: 'Gummy bears', inPantry: true, _id:123456},
-  {text: 'Sour Octopus', inPantry: true, _id:123457},
-  {text: 'Twizzlers', inPantry: false, _id:123458},
-]
-
-export {
-  candies
-}
